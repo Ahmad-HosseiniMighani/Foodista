554a1936b5de41e497066545c0dffaee92f24c69
Ahmad <13ahmad.hm78@gmail.com>
2020-06-15 09:42:53 +0430

    Updates

---
+++ precache-manifest.9f115f1ccdd227d31ac6d66b9f436756.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41b8c28bd408f0e15aa12d2fcb0518f",
+    "url": "/food-website/index.html"
+  },
+  {
+    "revision": "30d2bbb6966e3d52e5ae",
+    "url": "/food-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5c966cb6f620cf184ff9",
+    "url": "/food-website/static/js/2.f56476dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-website/static/js/2.f56476dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d2bbb6966e3d52e5ae",
+    "url": "/food-website/static/js/main.da9d8b35.chunk.js"
+  },
+  {
+    "revision": "4afdd018f21b7618e600",
+    "url": "/food-website/static/js/runtime-main.f6c3694d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/food-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
